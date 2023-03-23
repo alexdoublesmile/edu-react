333b8b1042b49794c1302aa6b9d1ff7653232d2c
Александр Плохой <alexplohoy@mail.ru>
2023-03-23 12:15:08 +0300

    add bootstrap styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import reactDom from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 // const element = React.createElement("h1", null, "Hello from React");
 const element = <h1>"Hello from React"</h1>;
