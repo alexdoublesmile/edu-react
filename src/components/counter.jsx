06f836cf582107e8ca8aafa5524ff52e3ab121e3
Александр Плохой <alexplohoy@mail.ru>
2023-03-23 18:02:08 +0300

    add jsx vars

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+const counter = 0;
+const formatCounter = () => counter === 0 ? "empty" : counter;
+
 const Counter = () => 
-<>
-    <h1>Counter</h1>
-    <button>+</button>
-</>;
+    <>
+        <h1>{formatCounter()}</h1>
+        <button>+</button>
+    </>;
 
 export default Counter;
