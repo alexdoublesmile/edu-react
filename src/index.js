06691d2689f626ee624187e80fed1164a9a1e27f
Александр Плохой <alexplohoy@mail.ru>
2023-03-23 12:12:22 +0300

    add custom elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import React from "react";
+import reactDom from "react-dom";
 
+// const element = React.createElement("h1", null, "Hello from React");
 const element = <h1>"Hello from React"</h1>;
+
+reactDom.render(element, document.querySelector("#root"));
+
 console.log(element);
