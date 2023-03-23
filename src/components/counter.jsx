afba72590c0aedb9294c740f33f6592c66cdac44
Александр Плохой <alexplohoy@mail.ru>
2023-03-23 18:20:30 +0300

    add styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const counter = 0;
 const formatCounter = () => counter === 0 ? "empty" : counter;
+const getBadgeColorClass = counter => counter === 0 ? "bg-warning" : "bg-primary";
+
+const badgeClasses = `badge ${getBadgeColorClass(counter)} m-2`;
+const buttonClasses = "btn btn-sm bg-primary m-2";
 
 const Counter = () => 
     <>
-        <h1>{formatCounter()}</h1>
-        <button>+</button>
+        <span className={badgeClasses}>{formatCounter()}</span>
+        <button className={buttonClasses}>+</button>
     </>;
 
 export default Counter;
