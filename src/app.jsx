d3c1231db05386f4f442d866ecf62704c6bcfb59
Александр Плохой <alexplohoy@mail.ru>
2023-03-24 17:22:45 +0300

    return counter app

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import Counter from "./components/counter";
+import Users from "./components/users";
+
+const App = () => <Counter />;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import reactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
-import Counter from "./components/counter";
-import Users from "./components/users";
-
-const App = () => <Users />;
+import App from "./app"
 
 reactDom.render(<App />, document.querySelector("#root"));
