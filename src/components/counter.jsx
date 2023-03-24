60fa138d6f0bf48dc1091d554d0b15638aee3850
Александр Плохой <alexplohoy@mail.ru>
2023-03-24 10:00:02 +0300

    add async component changing

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
 
     const formatCount = () => count === 0 ? "empty" : count;
     const getBadgeColorClass = () => count === 0 ? "bg-warning" : "bg-primary";
@@ -11,14 +12,34 @@ const Counter = () => {
     
     const handleIncrement = () => setCount(prevState => ++prevState);
     const handleDecrement = () => setCount(prevState => --prevState);
+    const handleRemoveTag = key => setTags(prevState => 
+        prevState.filter(tag => tag !== key));
+
+    const renderTags = () => {
+        if (tags.length > 0) {
+            return tags.map(tag => 
+                <li 
+                    key={tag} 
+                    className={buttonClasses} 
+                    onClick={() => handleRemoveTag(tag)}
+                >
+                    {tag}
+                </li>
+            );
+        }
+    };
     
-    return (
-        <>
+    return tags.length > 0 
+        ? <>
+            <ul>
+                {renderTags()}
+            </ul>
+        </> 
+        : <>
             <span className={badgeClasses}>{formatCount()}</span>
             <button className={buttonClasses} onClick={handleIncrement}>+</button>
             <button className={buttonClasses} onClick={handleDecrement}>-</button>
         </>
-    );  
 };
 
 export default Counter;
