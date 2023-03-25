00107d4369924ccbc6726f25d7520e925a07e8b5
Александр Плохой <alexplohoy@mail.ru>
2023-03-25 19:10:55 +0300

    add page numbers

--- src/components/users/pagination.jsx
+++ src/components/users/pagination.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import _ from "lodash";
 
 const Pagination = props => {
+    const { itemsCount, pageSize } = props;
+    const pagesCount = Math.ceil(itemsCount / pageSize)
+    const pages = _.range(1, pagesCount + 1);
+    
     return ( 
         <nav>
             <ul className="pagination">
-                <li className="page-item"><a className="page-link">1</a></li>
+                { pages.map(page => {
+                    return (
+                        <li className="page-item" key={"page_" + page}>
+                            <a className="page-link">{page}</a>
+                        </li>
+                    );
+                })}
             </ul>
         </nav>
      );
