52db622b6716e25555c0a7670ee8a996535a56d9
Александр Плохой <alexplohoy@mail.ru>
2023-03-25 19:54:57 +0300

    fix bookmarks logic

--- src/components/users/bookmark.jsx
+++ src/components/users/bookmark.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 
 const Bookmark = userProps => {
     return ( 
+        // <button 
+        //     onClick={() => userProps.onMark(userProps.id)}
+        // >
+        //     <i className={"bi bi-bookmark" + (userProps.isMarked ? "-heart-fill" : "")}></i>
+        // </button>
         <button 
+            type="button" 
+            className="btn btn-default btn-lg"
             onClick={() => userProps.onMark(userProps.id)}
         >
-            <i className={"bi bi-bookmark" + (userProps.isMarked ? "-heart-fill" : "")}></i>
+          <span className={"glyphicon" + (userProps.isMarked ? " glyphicon-bookmark" : "")}></span>
         </button>
      );
 }
