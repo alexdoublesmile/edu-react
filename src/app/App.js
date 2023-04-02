32ffa27edf43dc796037fb413eec09df134e4ce1
Александр Плохой <alexplohoy@mail.ru>
2023-04-02 11:48:51 +0300

    fix app

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
 import NavBar from "./components/users/navBar";
+import Users from "./layouts/users";
+import Login from "./layouts/login";
+import Main from "./layouts/main";
+import NotFound from "./components/users/notFound";
+import UserInfo from "./components/users/userInfo";
 
 const App = () => {
     return (
         <>
+            <NavBar />
             <Switch>
                 <Route exact path="/" component={Main} />
                 <Route path="/login" component={Login} />
@@ -12,7 +20,6 @@ const App = () => {
                 <Redirect to="/" />
                 <Route path="/404" component={NotFound} />
             </Switch>
-            <NavBar />
         </>
     );
 };
--- src/app/components/users/navBar.jsx
+++ src/app/components/users/navBar.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import Users from "../../layouts/users";
-import Login from "../../layouts/login";
-import Main from "../../layouts/main";
-import NotFound from "./notFound";
-import { Route, Switch, Redirect, Link } from "react-router-dom";
-import UserInfo from "./userInfo";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
