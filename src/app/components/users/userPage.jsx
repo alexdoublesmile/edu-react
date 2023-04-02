77c156424a3dfaf94b4ef305db913b6745ded1a3
Александр Плохой <alexplohoy@mail.ru>
2023-04-02 12:18:50 +0300

    fix history push

--- src/app/components/users/userPage.jsx
+++ src/app/components/users/userPage.jsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import api from "../../api";
 import QualitiesList from "./qualitiesList";
+import { useHistory } from "react-router-dom";
 
 const UserPage = ({ id }) => {
     const [user, setUser] = useState();
+    const history = useHistory();
 
     useEffect(() => {
         api.users.getById(id).then((data) => {
             setUser(data);
         });
     }, []);
+
+    const handleHistory = () => history.push("/users");
 
     if (user) {
         return (
@@ -22,8 +25,8 @@ const UserPage = ({ id }) => {
                     <QualitiesList qualities={user.qualities} />
                     <h3>Completed meetings: {user.completedMeetings}</h3>
                     <h3>Rate: {user.rate}</h3>
-                    <button>
-                        <Link to="/users">All users</Link>
+                    <button onClick={handleHistory}>
+                        All users
                     </button>
                 </>
             )
