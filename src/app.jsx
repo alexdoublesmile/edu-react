d28387cdce1b5df8594e84e731f0c8bc58ba05e5
Александр Плохой <alexplohoy@mail.ru>
2023-04-02 11:03:02 +0300

    add layots

--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,35 @@
 import React from "react";
-import NavBar from "./components/blog/navBar";
-import { Route, Switch, Redirect } from "react-router-dom";
-import Posts from "./components/blog/posts";
-import Dashboard from "./components/blog/dashboard";
-import Login from "./components/blog/login";
-import Home from "./components/blog/home";
-import NotFound from "./components/blog/notFound";
-// import Users from "./components/users/users";
+import NavBar from "./components/users/layouts/navBar";
+// import Posts from "./components/blog/posts";
+// import Dashboard from "./components/blog/dashboard";
+// import Login from "./components/blog/login";
+// import Home from "./components/blog/home";
 // import CounterList from "./components/counters/counterList";
 
 // const App = () => <CounterList />;
-// const App = () => <Users />;
-function App() {
+const App = () => {
     return (
         <>
             <NavBar />
-            <h1>App</h1>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/dashboard" component={Dashboard} />
-                <Redirect from="/admin" to="/dashboard" />
-                <Route path="/posts/:postId?" component={Posts} />
-                <Route path="/404" component={NotFound} />
-                <Redirect to="/404"/>
-            </Switch>
         </>
     );
-}
+};
+// function App() {
+//     return (
+//         <>
+//             <NavBar />
+//             <h1>App</h1>
+// <Switch>
+//     <Route exact path="/" component={Home} />
+//     <Route path="/login" component={Login} />
+//     <Route path="/dashboard" component={Dashboard} />
+//     <Redirect from="/admin" to="/dashboard" />
+//     <Route path="/posts/:postId?" component={Posts} />
+//     <Route path="/404" component={NotFound} />
+//     <Redirect to="/404"/>
+// </Switch>
+//         </>
+//     );
+// }
+
 export default App;
---
+++ src/components/users/layouts/navBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Users from "../users";
+import Login from "./login";
+import Main from "./main";
+import NotFound from "./notFound";
+import { Route, Switch, Redirect, Link } from "react-router-dom";
+import UserInfo from "./userInfo";
+
+const NavBar = () => {
+    return (
+        <>
+            <div>
+                <span><Link to="/main">Main</Link></span>{" "}
+                <span><Link to="/login">Login</Link></span>{" "}
+                <span><Link to="/users">Users</Link></span>{" "}
+            </div>
+            <Switch>
+                <Route path="/main" component={Main} />
+                <Route path="/login" component={Login} />
+                <Route exact path="/users" component={Users} />
+                <Route path="/users/:userId?" render={() => <UserInfo />} />
+                <Redirect from="/" to="/users" />
+                <Route path="/404" component={NotFound} />
+                <Redirect to="/404" />
+            </Switch>
+        </>
+    );
+};
+
+export default NavBar;
