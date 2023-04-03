9bebe43569f27b813cc7e82655a83789860c1a88
Александр Плохой <alexplohoy@mail.ru>
2023-04-03 18:15:49 +0300

    add form listener

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+    const [form, setForm] = useState({ email: "", password: "" });
+
+    const handleChange = ({ target }) => {
+        setForm((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+
     return (
         <form action="">
             <div>
                 <label htmlFor="email">E-mail</label>
-                <input type="text" id="email" />
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={form.email}
+                    onChange={handleChange}
+                />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={form.password}
+                    onChange={handleChange}
+                />
             </div>
         </form>
     );
