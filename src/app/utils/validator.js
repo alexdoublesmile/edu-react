ec300dedc239b01614783bb119443a0640a4eb83
Александр Плохой <alexplohoy@mail.ru>
2023-04-04 09:15:35 +0300

    add validation functionfix

--- src/app/utils/validator.js
+++ src/app/utils/validator.js
@@ -1,26 +1,29 @@
+function validate(validateMethod, data, config) {
+    switch (validateMethod) {
+    case "isRequired":
+        if (data.trim() === "") {
+            return config.message;
+        }
+        break;
+    default:
+        break;
+    }
+}
+
 export function validator(data, config) {
     const errors = {};
 
-    function validate(validateMethod, data, config) {
-        switch (validateMethod) {
-            case "isRequired":
-                if (data.trim() === "") {
-                    return config.message;
-                }
-                break;
-
-            default:
-                break;
-        }
-    }
-
     for (const fieldName in data) {
         for (const validateMethod in config[fieldName]) {
-            errors[fieldName] = validate(
+            const error = validate(
                 validateMethod,
                 data[fieldName],
                 config[fieldName][validateMethod]
             );
+
+            if (error) {
+                errors[fieldName] = error;
+            }
         }
     }
     return errors;
