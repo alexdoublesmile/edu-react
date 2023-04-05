ffc480a362f10a4232f0fadfa78adecfc1c4287f
Александр Плохой <alexplohoy@mail.ru>
2023-04-05 14:39:59 +0300

    fix routes

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import EditUserPage from "../components/page/editUserPage";
-import UserListPage from "../components/page/userListPage";
 import UserPage from "../components/page/userPage";
-
+import UserListPage from "../components/page/userListPage";
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
-
     return (
         <>
             {userId ? (
                 edit ? (
                     <EditUserPage />
                 ) : (
-                    <UserPage id={userId} />
+                    <UserPage userId={userId} />
                 )
             ) : (
                 <UserListPage />
