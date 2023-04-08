4ac934812edf7dafa06bcf38325c086b439ac259
Александр Плохой <alexplohoy@mail.ru>
2023-04-08 16:53:09 +0300

    fix user card

--- src/app/components/ui/userCard.jsx
+++ src/app/components/ui/userCard.jsx
@@ -11,19 +11,19 @@ const UserCard = ({ user }) => {
     return (
         <div className="card mb-3">
             <div className="card-body">
-                <button 
+                <button
                     className="position-absolute top-0 end-0 btn btn-light btn-sm"
                     onClick={handleClick}
                 >
                     <i className="bi bi-gear"></i>
                 </button>
                 <div className="d-flex flex-column align-items-center text-center position-relative">
-                    <img 
+                    <img
                         src={`https://avatars.dicebear.com/api/avataaars/${(
                             Math.random() + 1
                         )
                             .toString(36)
-                            .substring(7)}.svg`} 
+                            .substring(7)}.svg`}
                         className="rounded-circle"
                         width="150"
                     />
@@ -33,11 +33,11 @@ const UserCard = ({ user }) => {
                             {user.profession.name}
                         </p>
                         <div className="text-muted">
-                            <i 
-                                className="bi bi-caret-down-fill text-primary" 
+                            <i
+                                className="bi bi-caret-down-fill text-primary"
                                 role="button"
                             ></i>
-                            <i 
+                            <i
                                 className="bi bi-caret-up text-secondary" 
                                 role="button"
                             ></i>
