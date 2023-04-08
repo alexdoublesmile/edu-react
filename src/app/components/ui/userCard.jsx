63d250065d03dda27e743e3989f508ee6cde6b74
Александр Плохой <alexplohoy@mail.ru>
2023-04-08 15:29:23 +0300

    fix user card

--- src/app/components/ui/userCard.jsx
+++ src/app/components/ui/userCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 
 const UserCard = ({ user }) => {
     const history = useHistory();
