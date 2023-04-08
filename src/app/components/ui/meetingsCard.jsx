429e6ed724bca15a995dc757b7a3fb73b10765d6
Александр Плохой <alexplohoy@mail.ru>
2023-04-08 15:45:08 +0300

    add meetings card

--- src/app/components/ui/meetingsCard.jsx
+++ src/app/components/ui/meetingsCard.jsx
@@ -3,12 +3,20 @@ import PropTypes from "prop-types";
 
 const MeetingsCard = ({ value }) => {
     return (
-        null
+        <div className="card-mb-3">
+            <div className="card-body d-flex flex-column justify-content-center text-center">
+                <h5 className="card-title">
+                    <span>Completed meetings</span>
+                </h5>
+                
+                <h1 className="display-1">{value}</h1>
+            </div>
+        </div>
     );
-}
+};
 
 MeetingsCard.propTypes = {
-    user: PropTypes.object
+    value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]) 
 };
  
 export default MeetingsCard;
