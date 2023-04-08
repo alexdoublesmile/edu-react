6563b8bb8d6fe80066a4c19753badedda8b5e4be
Александр Плохой <alexplohoy@mail.ru>
2023-04-08 15:34:01 +0300

    add qualities card

--- src/app/components/ui/qualitiesCard.jsx
+++ src/app/components/ui/qualitiesCard.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Qualities from "./qualities";
 
 const QualitiesCard = ({ data }) => {
     return (
-        null
+        <div className="card mb-3">
+            <div className="card-body d-flex flex-column justify-content-center text-center">
+                <h5 className="card-title">
+                    <span>Qualities</span>
+                </h5>
+                <p className="card-text">
+                    <Qualities qualities={data} />
+                </p>
+            </div>
+        </div>
     );
-}
+};
 
 QualitiesCard.propTypes = {
-    user: PropTypes.object
+    data: PropTypes.array
 };
  
 export default QualitiesCard;
