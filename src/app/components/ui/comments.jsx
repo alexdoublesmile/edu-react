e3a4685aa7785b8eaca754822861ee05e71ae9ff
Александр Плохой <alexplohoy@mail.ru>
2023-04-08 17:29:17 +0300

    fix comments

--- src/app/components/ui/comments.jsx
+++ src/app/components/ui/comments.jsx
@@ -1,55 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import api from "../../api";
 import { orderBy } from "lodash";
+import React, { useEffect, useState } from "react";
+import api from "../../api";
+import { useParams } from "react-router-dom";
 import CommentsList, { AddCommentForm } from "../common/comments";
 
 const Comments = () => {
     const { userId } = useParams();
     const [comments, setComments] = useState([]);
-
     useEffect(() => {
         api.comments
             .fetchCommentsForUser(userId)
             .then((data) => setComments(data));
     }, []);
-
     const handleSubmit = (data) => {
         api.comments
             .add({ ...data, pageId: userId })
             .then((data) => setComments([...comments, data]));
     };
-
     const handleRemoveComment = (id) => {
-        api.comments
-            .remove(id)
-            .then(id => {
-                setComments(comments.filter(comment => comment.id !== id));
-            });
+        api.comments.remove(id).then((id) => {
+            setComments(comments.filter((x) => x._id !== id));
+        });
     };
-
     const sortedComments = orderBy(comments, ["created_at"], ["desc"]);
-
     return (
         <>
             <div className="card mb-2">
                 {" "}
-                <div className="card-body">
+                <div className="card-body ">
                     <AddCommentForm onSubmit={handleSubmit} />
                 </div>
-                {sortedComments.length > 0 && (
-                    <div className="card mb-3">
-                        <div className="card-body ">
-                            <h2>Comments</h2>
-                            <hr />
-                            <CommentsList
-                                comments={sortedComments}
-                                onRemove={handleRemoveComment}
-                            />
-                        </div>
-                    </div>
-                )}
             </div>
+            {sortedComments.length > 0 && (
+                <div className="card mb-3">
+                    <div className="card-body ">
+                        <h2>Comments</h2>
+                        <hr />
+                        <CommentsList
+                            comments={sortedComments}
+                            onRemove={handleRemoveComment}
+                        />
+                    </div>
+                </div>
+            )}
         </>
     );
 };
