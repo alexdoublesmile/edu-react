6029d1390be289cbb12cbf2f221ce3fb6aca63b2
Александр Плохой <alexplohoy@mail.ru>
2023-04-09 15:06:24 +0300

    fix users

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import EditUserPage from "../components/page/editUserPage";
 import UserPage from "../components/page/userPage";
-import UserListPage from "../components/page/userListPage";
+import UsersListPage from "../components/page/userListPage";
 import UserProvider from "../hooks/useUsers";
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
     return (
-        <UserProvider>
-            {userId ? (
-                edit ? (
-                    <EditUserPage />
+        <>
+            <UserProvider>
+                {userId ? (
+                    edit ? (
+                        <EditUserPage />
+                    ) : (
+                        <UserPage userId={userId} />
+                    )
                 ) : (
-                    <UserPage userId={userId} />
-                )
-            ) : (
-                <UserListPage />
-            )}
-        </UserProvider>
+                    <UsersListPage />
+                )}
+            </UserProvider>
+        </>
     );
 };
 
