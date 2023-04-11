01c45e3dc0719164d4a4cafad44234748d22efb0
Александр Плохой <alexplohoy@mail.ru>
2023-04-11 12:31:16 +0300

    add ath info storage

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -2,25 +2,33 @@ import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 
+const httpAuth = axios.create();
 const AuthContext = React.createContext();
 
 export const useAuth = () => {
     return useContext(AuthContext);
 };
 
-// const TOKEN_KEY = "jwt-token";
-// const REFRESH_KEY = "jwt-refresh-token";
-// const EXPIRES_KEY = "jwt-expires";
+const TOKEN_KEY = "jwt-token";
+const REFRESH_KEY = "jwt-refresh-token";
+const EXPIRES_KEY = "jwt-expires";
 
 const AuthProvider = ({ children }) => {
+    function setTokens({ refreshToken, idToken, expiresIn = 3600 }) {
+        const expires = new Date().getTime() + expiresIn * 1000;
+        localStorage.setItem(TOKEN_KEY, idToken);
+        localStorage.setItem(REFRESH_KEY, refreshToken);
+        localStorage.setItem(EXPIRES_KEY, expires);
+    }
     async function signUp({ email, password }) {
         const key = "AIzaSyCDxYiBYc2eWs4vkpSAKdaWHs3rVh_VGs0";
         const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`;
-        const { data } = await axios.post(url, {
+        const { data } = await httpAuth.post(url, {
             email,
             password,
             returnSecureToken: true
         });
+        setTokens(data);
         console.log("Auth data: ", data);
     };
 
