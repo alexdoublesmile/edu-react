671ad64c3d962e37c754970ae84189e30d41ac9f
Александр Плохой <alexplohoy@mail.ru>
2023-04-11 12:34:18 +0300

    add user create

--- src/app/services/user.service.js
+++ src/app/services/user.service.js
@@ -3,8 +3,15 @@ import httpService from "./http.service";
 const userEndpoint = "user/";
 
 const userService = {
-    get: async() => {
+    get: async () => {
         const { data } = await httpService.get(userEndpoint);
+        return data;
+    },
+    create: async (payload) => {
+        const { data } = await httpService.put(
+            userEndpoint + payload._id,
+            payload
+        );
         return data;
     }
 };
