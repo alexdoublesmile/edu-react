886be85b10fc266723042ca85162a44ca4b10179
Александр Плохой <alexplohoy@mail.ru>
2023-04-12 08:51:11 +0300

    add data to user

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -42,6 +42,10 @@ const AuthProvider = ({ children }) => {
         }
     }
 
+    function randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
     async function signUp({ email, password, ...rest }) {
         try {
             const { data } = await httpAuth.post("accounts:signUp", {
@@ -51,7 +55,7 @@ const AuthProvider = ({ children }) => {
             });
 
             setTokens(data);
-            await createUser({ _id: data.localId, email, ...rest });
+            await createUser({ _id: data.localId, email, rate: randomInt(1, 5), completedMeetings: randomInt(0, 200), ...rest });
             console.log("Sign up data: ", data);
         } catch (error) {
             errorCatcher(error);
