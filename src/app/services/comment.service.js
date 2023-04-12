1049cdc2ba02c821336919219207651b8ce67924
Александр Плохой <alexplohoy@mail.ru>
2023-04-12 15:53:10 +0300

    add nano id

--- src/app/services/comment.service.js
+++ src/app/services/comment.service.js
@@ -2,23 +2,23 @@ import httpService from "./http.service";
 const commentEndpoint = "comment/";
 
 const commentService = {
-    createComment: async (payload) => {
+    createComment: async(payload) => {
         const { data } = await httpService.put(
             commentEndpoint + payload._id,
             payload
         );
         return data;
     },
-    getComments: async (pageId) => {
+    getComments: async(pageId) => {
         const { data } = await httpService.get(commentEndpoint, {
             params: {
-                orderBy: '"pageId"',
+                orderBy: "pageId",
                 equalTo: `"${pageId}"`
             }
         });
         return data;
     },
-    removeComment: async (commentId) => {
+    removeComment: async(commentId) => {
         const { data } = await httpService.delete(commentEndpoint + commentId);
         return data;
     }
