14391b1f1f093941caa4e752fcf170d6548ce8b0
Александр Плохой <alexplohoy@mail.ru>
2023-04-17 17:55:23 +0300

    add posts

---
+++ lazy/src/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Posts = () => {
+    return ( 
+        <>
+            <h3>Posts</h3>
+            <ul>
+
+            </ul>
+        </>
+    );
+}
+ 
+export default Posts;
