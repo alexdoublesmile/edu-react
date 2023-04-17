d2ee1383f2f50b328a1d712a1c23bda1a455cf0a
Александр Плохой <alexplohoy@mail.ru>
2023-04-17 17:45:38 +0300

    add use resource

--- lazy/src/resource.js
+++ lazy/src/resource.js
@@ -1,3 +1,9 @@
+export function useResource() {
+    return {
+        posts: fetchPosts()
+    }
+};
+
 function fetchPosts() {
     return fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
         .then(resp => resp.json());
