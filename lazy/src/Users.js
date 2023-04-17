75485f5c819bd047760433b1935dcece77b61b87
Александр Плохой <alexplohoy@mail.ru>
2023-04-17 18:09:22 +0300

    add users js

---
+++ lazy/src/Users.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Users = ({ resource }) => {
+    const users = resource.users.read();
+
+    return (
+        <>
+            <h3>Users</h3>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default Users;
